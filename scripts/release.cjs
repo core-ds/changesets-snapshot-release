367a79d8f0d93ba4f2a295217b285e7292c92a1b
hextion <100ishundred@gmail.com>
2025-08-19 07:42:58 +0300

    fix: correct release

--- scripts/release.cjs
+++ scripts/release.cjs
@@ -1,19 +1,40 @@
 // @ts-check
 
+// copied from https://github.com/changesets/action/blob/04d574e831923498156e0b2b93152878063203a3/scripts/release.ts
+
 const { version } = require("../package.json");
-const { exec } = require("@actions/exec");
+const { exec, getExecOutput } = require("@actions/exec");
 
 async function main() {
+  const tag = `v${version}`;
+
+  const { exitCode, stderr } = await getExecOutput(
+    `git`,
+    ["ls-remote", "--exit-code", "origin", "--tags", `refs/tags/${tag}`],
+    { ignoreReturnCode: true }
+  );
+  if (exitCode === 0) {
+    console.log(`Action is not being published because version ${tag} is already published`);
+    return;
+  }
+  /**
+   * Exit with status "2" when no matching refs are found in the remote repository.
+   * @see https://git-scm.com/docs/git-ls-remote#Documentation/git-ls-remote.txt---exit-code
+   */
+  if (exitCode !== 2) {
+    throw new Error(`git ls-remote exited with ${exitCode.toString()}:\n${stderr}`);
+  }
+
   await exec("git", ["checkout", "--detach"]);
   await exec("git", ["add", "--force", "dist"]);
   await exec("git", ["commit", "-m", `publish: v${version}`]);
 
-  // needs to work, git tag itself is not used
   await exec("changeset", ["tag"]);
 
   const [major] = version.split(".");
+  const branch = `v${major}`;
 
-  await exec("git", ["push", "--force", "origin", `HEAD:refs/heads/v${major}`]);
+  await exec("git", ["push", "--force", "--follow-tags", "origin", `HEAD:refs/heads/${branch}`]);
 }
 
 void main();
